8cd502805442894ce2ea24224aa8c9a6de4f1a17
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-03-07 12:31:12 -0600

    adding request tests

---
+++ spec/factories/todos.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :todo do
+    title { Faker::Lorem.word }
+    created_by { Faker::Number.number(digits: 10) }
+  end
+end
+
+# By wrapping Faker methods in a block, we ensure that Faker generates dynamic data every time the factory is invoked.
